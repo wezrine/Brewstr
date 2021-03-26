7482a6a5ac372939daa6021f96b14f8c602243bf
wezrine <77981551+wezrine@users.noreply.github.com>
2021-03-26 15:08:06 -0400

    view/server

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const mustacheExpress = require('mustache-express')
+
+// const models = require('./models')
+// const { Op } = require('sequelize')
+
+// Mustache Express
+app.engine('mustache', mustacheExpress())
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+// Body Parser
+app.use(express.urlencoded())
+
+// Load CSS & JS
+app.use(express.static('css'))
+app.use(express.static('scripts'))
+app.use(express.static('images'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/listings', (req, res) => {
+    res.render('listings')
+})
+
+// Launch Server
+app.listen(3000, () => {
+    console.log('Server is running...')
+})
