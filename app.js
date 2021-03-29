dbd8f2e878f84280ef54805b2eaa657b5556357c
jorge_cuza <jorge.cuza92@gmail.com>
2021-03-29 11:20:41 -0500

    setting up registration and login

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const mustacheExpress = require('mustache-express')
+const session = require('express-session')
+var bcrypt = require('bcryptjs')
+const pgp = require('pg-promise')
+
+const db =
 
 // const models = require('./models')
 // const { Op } = require('sequelize')
@@ -18,9 +23,34 @@ app.use(express.static('css'))
 app.use(express.static('scripts'))
 app.use(express.static('images'))
 
+// Setting up session
+app.use(session({
+    secret: 'handmeabeer',
+    saveUnitialized: true
+}))
+
 // Routes
 const loginRouter = require('./routes/login')
 app.use('/login', loginRouter)
+
+// user registration (we can move to route soon)
+app.get('/register', (req,res) => {
+    res.render('register')
+})
+
+app.post('/register', (req,res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    bcrypt.genSalt
+})
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
     res.render('home')
