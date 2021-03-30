29eaafb086939330e66e7a5b6caff16383b23689
SJ-CODES <summerjohnson@Summers-Air.attlocal.net>
2021-03-30 10:02:27 -0400

    updated reviews able to post review to template and store in database

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const fetchBreweryById = require('./scripts/fetchById.js')
 
 // const db =
 
-// const models = require('./models')
-// const { Op } = require('sequelize')
+const models = require('./models')
+const { Op } = require('sequelize')
 // const session = require('express-session')
 
 // Mustache Express
@@ -68,6 +68,50 @@ app.get('/brewery/:brewery', (req, res) => {
 
     res.render('brewery_details')
 })
+
+app.get('/brewery_details', (req, res) => {
+    res.render('brewery_details')
+})
+
+app.get('/add-review', (req, res) => {
+    models.Reviews.findAll({})
+    .then(Reviews => {
+        res.redirect('/add-review', {reviews: reviews})
+    })   
+})
+app.post('/add-review', (req, res) =>{
+    const breweryId = req.body.fetchBreweryById
+    const name = req.body.name
+    const review = req.body.review
+
+    let reviews = models.Review.build({
+        name:name,
+        review:review,
+        breweryId: breweryId
+
+    })
+    reviews.save().then(savedReviews=>{
+        console.log(savedReviews)
+        res.redirect(`/brewery/${savedReviews.breweryId}`)
+    }).catch((error) =>{
+        console.log(error)
+        res.send('comment not added')
+    })
+    
+})
+
+app.post('/delete-review', (req, res) => {
+    const ReviewsId = req.body.ReviewsId
+
+    models.Blogs.destroy({
+        where: {
+            id: ReviewsId 
+        }
+    }).then(deletedReviews => {
+        res.redirect(req.get('referer'))
+    })
+})
+
 
 // Load reviews page and adds a review
 
