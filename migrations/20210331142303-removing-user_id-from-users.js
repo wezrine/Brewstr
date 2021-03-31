7ec8b8c81d83566bc1c5fab75b0afda51b4aef00
SJ-CODES <summerjohnson@Summers-Air.attlocal.net>
2021-03-31 14:22:24 -0400

    able to add and show reviews

--- migrations/20210331142303-removing-user_id-from-users.js
+++ migrations/20210331142303-removing-user_id-from-users.js
@@ -2,12 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    return queryInterface.removeColumn('Users', 'user_id')
   },
 
   down: async (queryInterface, Sequelize) => {
